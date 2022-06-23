d6e3005e6647e7ccd35cfd86c36c9b8a6c894ca2
bruno felipe <bruno.f.martins1996@gmail.com>
2022-06-23 20:49:59 -0300

    console.log

--- JavaScript/teste.js
+++ JavaScript/teste.js
@@ -3,9 +3,11 @@ function returnEvenValues(array){
     for(let i = 0; i < array.length; i++) {
         if(array[i] % 2 === 0) {
             evenNums.push(array[i]);
+        } else {
+            console.log(`${array[i]} não é par!`);
         }
     }
-    console.log(evenNums);
+    console.log("os números pares são:",evenNums);
 }
 
 let array = [1, 2, 4, 5, 7, 8]
